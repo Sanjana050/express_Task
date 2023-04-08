a184ceebbbbee19a17c05be5ee3a3370a313b0d7
Sanjana <sanjanamondaltffs@gmail.com>
2023-04-08 10:08:56 +0530

    second commit

--- task1.js
+++ task1.js
@@ -1,11 +1,23 @@
 const express=require('express');
 const app=express();
-app.use((req,res,next)=>{
-    console.log('response');
-   next();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+app.use('/add-product',(req,res,next)=>{
+    
+   
+   res.send(`<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>`)
+
 })
-app.use((req,res,next)=>{
-    console.log('another response');
+
+app.post('/product',(req,res,next)=>{
+    console.log(`name:${req.body.title} and size:${req.body.size}`);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+   
+    
     res.send(  `<h1>Hello from express js</h1>`)
 })
 

06907cc6d743a243c444c882473e916b4907ce39
Sanjana <sanjanamondaltffs@gmail.com>
2023-04-08 11:09:00 +0530

    third

--- task1.js
+++ task1.js
@@ -4,21 +4,14 @@ const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}))
 
 
-app.use('/add-product',(req,res,next)=>{
-    
-   
-   res.send(`<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>`)
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 
+app.use('/admin',adminRoutes)
+app.use('/shop',shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send(`<h1>Page not found</h1>`)
 })
 
-app.post('/product',(req,res,next)=>{
-    console.log(`name:${req.body.title} and size:${req.body.size}`);
-    res.redirect('/');
-})
-app.use('/',(req,res,next)=>{
-   
-    
-    res.send(  `<h1>Hello from express js</h1>`)
-})
 
-app.listen(80);
+app.listen(3000);
